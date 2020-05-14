dc70fbd4cd08c22e31c8843acafc2b43b0dedccd
ElisaGiannozzi <elisa.giannozzi@gmail.com>
2020-05-14 18:46:11 +0200

    created nav bar + slider

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.slider-container').slick({
-        arrows: true,
-        dots: true,
-        prevArrow: '<button class="arrow prev-arrow"><i class="fas fa-chevron-left"></i></button>',
-        nextArrow: '<button class="arrow next-arrow"><i class="fas fa-chevron-right"></i></button>'
+      arrows: false, 
+      dots: true, 
   });
 });
