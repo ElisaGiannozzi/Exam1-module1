5aa18c46b1b44e2c972bd44d3a1eee13fe3df6d3
ElisaGiannozzi <elisa.giannozzi@gmail.com>
2020-05-16 20:31:48 +0200

    created section 3

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
       dots: true, 
   });
 });
+
+
+$(document).ready(function(){
+    $('.slider-container2').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: true, 
+      dots: true
+    });
+  });
